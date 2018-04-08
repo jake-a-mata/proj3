62ab4f061563eed3f1150c43e2e8a7a1bcdf0e94
jake-a-mata <jake.mata22@gmail.com>
2018-04-08 12:31:57 -0500

    initial setup for mysh.c

---
+++ mysh.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+void mycat() {
+
+}
+
+
+void mycp() {
+
+}
+
+
+void myls() {
+
+}
+
+
+void mycd() {
+
+}
+
+
+void pwd() {
+
+}
+
+
+int main(int argc, char** argv) {
+
+
+
+  return 0; 
+}
