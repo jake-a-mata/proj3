e8e1fa55dbc95c015f5edbbd48ce45f14bb03403
RyanHoward84 <reh101@txstate.edu>
2018-04-12 14:30:00 -0500

    Working pwd function

--- mysh.c
+++ mysh.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-
+#include <unistd.h>
 
 void mycat() {
 
@@ -23,13 +23,28 @@ void mycd() {
 }
 
 
-void pwd() {
+char* pwd() {
+	
+	char *ptr;
+ 	char currentDir[100];
 
+ 	if (getcwd(currentDir, sizeof(currentDir)) != NULL) {
+		printf("Current working dir: %s\n", currentDir);
+		ptr = (char*)malloc(sizeof(currentDir));
+		ptr = currentDir;
+		return ptr;
+	} 
+   
 }
 
 
 int main(int argc, char** argv) {
 
+   char *buf;  
+
+   buf = pwd();
+
+   printf("%s",buf);
 
 
   return 0; 
