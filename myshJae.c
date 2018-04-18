fd3f19f3ac03c889803ca4a8cfbacd7d751fcfb7
ksy903 <ksy903@gmail.com>
2018-04-18 15:46:14 -0500

    Added line by line input detection

--- myshJae.c
+++ myshJae.c
@@ -39,8 +39,49 @@ char* pwd() {
 
 int main(int argc, char** argv) {
     char *input;
-
     while(1){
-        scanf("", input);
+        input = (char *)malloc(1);
+        input[0] = '\0';
+
+        while(1){
+            char c;
+            scanf("%c", &c);
+            if(c == '\n'){
+                break;
+            }
+            else{
+                char *word = (char *)malloc(sizeof(c) + 1);
+                word[0] = c;
+                word[1] = '\0';
+                strcat(input, word);
+            }
+        }
+
+        printf("%s\n", input);
+
+        if(strcmp(input, "exit") == 0)break;
+
+        char **separate;
+        char *word;
+
+        int i;
+        for(i=0;i<strlen(input);i++){
+            word = (char *)malloc(1);
+            word[0] = '\0';
+
+            if(input[i] == ' '){
+                separate = (char **)malloc(sizeof(separate) + sizeof(word) + 1);
+            }
+            else if(input[i] == '\0');
+            else{
+                char *tmp;
+                tmp = (char *)malloc(sizeof(input[i]) + 1);
+                tmp[0] = input[i];
+                tmp[1] = '\0';
+                strcat(word, tmp);
+            }
+        }
+
+        free(input);
     }
 }
